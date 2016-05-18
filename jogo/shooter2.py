22aa9e72176073381152790b1a227242f68afd3d
AntonioJC <antli3@hotmail.com>
2016-05-18 16:23:43 +0100

    novo nível

--- jogo/shooter2.py
+++ jogo/shooter2.py
@@ -487,6 +487,47 @@ class shoot:
 
                 return simpos
 
+        def magkutta(self, screen, shot,B,Ex,Ey):                
+
+                # temos de relacionar os data types do c++ com os do python, entao identifica-se abaixo o tipo de cada argumento enviado para a funcao FullRK4 da biblioteca a para fazer esta conexao
+                # ----> Ver data-types em: https://docs.python.org/2/library/ctypes.html#fundamental-data-types !!
+                a.MagField.argtypes = [ ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double, ctypes.c_double] 
+
+                # o mesmo, mas agora para o retorno da funcao
+                a.MagField.restype = ctypes.POINTER(ctypes.c_double)
+                
+
+		end_point_x = self.sh_length*cos(self.sh_angle)
+		end_point_y = self.sh_length*sin(self.sh_angle)
+		
+		# faz-se o teste de se a bola acabou de ser disparada, ou seja, para ver se e a primeira vez que a funcao esta a ser chamada de forma 
+		# a actualizar a posicao da bola dependendo do angulo do shooter
+        
+                vel = 10
+		if(shot==True):
+			self.ball_pos_x0 = self.sh_pos_x + end_point_x 
+			self.ball_pos_y0 = self.sh_pos_y - end_point_y
+                        self.ball_pos_x = self.ball_pos_x0
+                        self.ball_pos_y = self.ball_pos_y0
+                        self.ball_vx = vel*cos(self.sh_angle)
+                        #self.ball_vy = vel*sin(self.sh_angle)
+                        self.ball_vy=0
+			self.t=0
+			
+		pygame.draw.circle(screen, GREEN, (int(self.ball_pos_x), int(self.ball_pos_y)), 5, 5)
+		
+
+                # chamar a funcao do c++
+
+                h = 0.5 #step
+                pos = a.MagField(h,self.ball_pos_x,self.ball_pos_y,self.ball_vx,self.ball_vy,B)
+
+		self.t=self.t+h
+		self.ball_pos_x = pos[0] 
+		self.ball_pos_y = pos[1]
+                self.ball_vx= pos[2]
+                self.ball_vy=pos[3]
+
         """
         def collisions(self, screen, shot, hcol,velcol):
         
